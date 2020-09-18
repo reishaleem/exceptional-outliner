5f782d17a948c14a8afa77dcb96aa417162a7577
ReisHaleem <Reis.Haleem@target.com>
2020-09-18 10:30:00 -0400

    serve static build

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const mongoose = require("mongoose");
 
 require("dotenv").config();
@@ -7,7 +8,14 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(express.static(path.join(__dirname, "app/build")));
+
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    })
+);
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
@@ -20,6 +28,10 @@ connectionPool.once("open", () => {
 const userRouter = require("./server/routes/UserRouter");
 
 app.use("/users", userRouter);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "app/build", "index.html"));
+});
 
 app.listen(port, () => {
     console.log("server is running on port", port);
