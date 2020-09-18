fbe7f22e9d70007097569423c12345ff4971c1b7
ReisHaleem <Reis.Haleem@target.com>
2020-09-18 17:40:04 -0400

    add second axios test

--- app/src/App.js
+++ app/src/App.js
@@ -4,16 +4,24 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-    function handleClick() {
+    function handleClick1() {
         axios
-            .get("users")
+            .get("http://0.0.0.0:5000/users")
+            .then((res) => console.log(res.data))
+            .catch((err) => console.log(err));
+    }
+
+    function handleClick2() {
+        axios
+            .get("http://0.0.0.0:5000/health")
             .then((res) => console.log(res.data))
             .catch((err) => console.log(err));
     }
     return (
         <div className="App">
             <header className="App-header">
-                <button onClick={handleClick}>Hi</button>
+                <button onClick={handleClick1}>Hi</button>
+                <button onClick={handleClick2}>Hi2</button>
             </header>
         </div>
     );
