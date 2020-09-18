bdfaaff61d0279b9dd6d5ecf4e59e1c8c81a5599
ReisHaleem <Reis.Haleem@target.com>
2020-09-18 10:43:54 -0400

    fix cors origin

--- server.js
+++ server.js
@@ -10,12 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "app/build")));
 
-app.use(
-    cors({
-        credentials: true,
-        origin: process.env.FRONTEND_URL,
-    })
-);
+app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
