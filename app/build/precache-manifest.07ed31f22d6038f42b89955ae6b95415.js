5c1d10f8042238d7db0e471760b2e32bdbb2002e
ReisHaleem <Reis.Haleem@target.com>
2020-09-19 10:09:14 -0400

    add build;

---
+++ app/build/precache-manifest.07ed31f22d6038f42b89955ae6b95415.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf9bb72e55d316d8842b259c040c8717",
+    "url": "/index.html"
+  },
+  {
+    "revision": "491ae499fb2212152bce",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ed8ed40ad94bdd86350",
+    "url": "/static/js/2.1bcda899.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1bcda899.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "491ae499fb2212152bce",
+    "url": "/static/js/main.514af1cd.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
