05d403b245364ef0257898e91051ff74c2aec5b2
ReisHaleem <Reis.Haleem@target.com>
2020-09-19 11:15:44 -0400

    fix index path

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get("/api/health", (req, res) => {
 });
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "app", "build", "index.html"));
 });
 
 app.listen(port, () => {
