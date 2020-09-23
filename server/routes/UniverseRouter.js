7fb00adc2ba3e25b37c635380978edaa4a7f8e0a
ReisHaleem <Reis.Haleem@target.com>
2020-09-22 20:26:54 -0400

    add universe update method

--- server/routes/UniverseRouter.js
+++ server/routes/UniverseRouter.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const universeService = require("../services/UniverseService");
 let User = require("../models/UserSchema");
+const { route } = require("./UserRouter");
 
 router.route("/").get((req, res) => {
     User.find()
@@ -13,6 +14,15 @@ router.route("/").get((req, res) => {
 
 router.route("/:id/create").post((req, res) => {
     universeService.createUniverse(req.params.id, req.body, res);
+});
+
+router.route("/:id/update/:universeId").put((req, res) => {
+    universeService.updateUniverse(
+        req.params.id,
+        req.params.universeId,
+        req.body,
+        res
+    );
 });
 
 module.exports = router;
--- server/services/UniverseService.js
+++ server/services/UniverseService.js
@@ -4,20 +4,6 @@ async function createUniverse(ownerId, request, response) {
     const name = request.name;
     const description = request.description;
 
-    // User.findById(ownerId)
-    //     .then((user) => {
-    //         user.universes.push({
-    //             name: name,
-    //             description: description,
-    //         });
-    //         user.save().then(() =>
-    //             response.json(
-    //                 `created universe for user with username username "${user.username}" !`
-    //             )
-    //         );
-    //     })
-    //     .catch((err) => response.status(400).json("Error: " + err));
-
     const user = await User.findById(ownerId).exec();
     if (user == null) {
         response.status(400).json("Error: user id does not exist");
@@ -37,6 +23,30 @@ async function createUniverse(ownerId, request, response) {
         .catch((err) => response.status(400).json("Error: " + err));
 }
 
+async function updateUniverse(ownerId, universeId, request, response) {
+    const name = request.name;
+    const description = request.description;
+
+    const user = await User.findById(ownerId).exec();
+    if (user == null) {
+        response.status(400).json("Error: user id does not exist");
+        return;
+    }
+
+    const universe = user.universes.id(universeId);
+    universe.name = name;
+    universe.description = description;
+
+    user.save()
+        .then(() =>
+            response.json(
+                `updated universe for user with username username "${user.username}" !`
+            )
+        )
+        .catch((err) => response.status(400).json("Error: " + err));
+}
+
 module.exports = {
     createUniverse,
+    updateUniverse,
 };
