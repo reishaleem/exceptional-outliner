fd7e246688e7d0cc7284956664101b9250026c2e
ReisHaleem <Reis.Haleem@target.com>
2020-10-14 21:52:23 -0400

    add material ui, add background image on home

--- app/src/components/pages/public/Homepage/Homepage.tsx
+++ app/src/components/pages/public/Homepage/Homepage.tsx
@@ -1,5 +1,47 @@
+import { makeStyles } from "@material-ui/core";
 import React from "react";
 
+import TestImage from "../../../../images/landing-bg.jpg"
+
+const useStyles = makeStyles((theme) => ({
+    background: {
+        
+        height: "90vh",
+        maxHeight: "1000px",
+        overflow: "hidden",
+        position: "relative",
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+        margin: "0",
+        padding: "0",
+        border: "0",
+        display: "flex",
+        alignItems: "center"
+    },
+    filter: {
+        "&:before": {
+          background: "rgba(0, 0, 0, 0.5)"
+        },
+        "&:after,&:before": {
+          position: "absolute",
+          zIndex: "1",
+          width: "100%",
+          height: "100%",
+          display: "block",
+          left: "0",
+          top: "0",
+          content: "''"
+        }
+      },
+}));
+
 export default () => {
-    return <h1>Test</h1>;
+    const classes = useStyles();
+    return (
+        <>
+            <div className={`${classes.background} ${classes.filter}`} style={{backgroundImage: "url(" + TestImage + ")"}}>
+
+            </div>
+        </>
+    )
 };
