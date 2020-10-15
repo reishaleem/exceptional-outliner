514ea6463a5cf1c79dd76b9010ce0ffb3ae96011
ReisHaleem <Reis.Haleem@target.com>
2020-10-15 13:59:32 -0400

    add main section and cards to home page

--- app/src/components/pages/public/Homepage/Homepage.tsx
+++ app/src/components/pages/public/Homepage/Homepage.tsx
@@ -1,4 +1,5 @@
-import { Button, Container, Grid, makeStyles, Typography } from "@material-ui/core";
+import { Button, Card, CardContent, Container, Grid, makeStyles, Typography } from "@material-ui/core";
+import BuildIcon from "@material-ui/icons/Build"
 import React from "react";
 
 import TestImage from "../../../../images/landing-bg.jpg"
@@ -43,7 +44,31 @@ const useStyles = makeStyles((theme) => ({
     },
     subtitle: {
         color: "#FFFFFF"
-    }
+    },
+    main: {
+        background: "#FFFFFF",
+        position: "relative",
+        zIndex: 3
+      },
+    mainRaised: {
+        margin: "-60px 30px 0px",
+        borderRadius: "6px",
+        boxShadow:
+            "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
+    },
+    section: {
+        padding: "70px 0",
+        textAlign: "center"
+    },
+    productTitle: {
+        marginBottom: "1rem",
+        marginTop: "30px",
+        minHeight: "32px",
+        textDecoration: "none"
+      },
+      productDescription: {
+        color: "#999"
+      }
 }));
 
 export default () => {
@@ -58,6 +83,53 @@ export default () => {
                         <Button color="secondary" variant="contained" size="large">Start your journey</Button>
                     </Grid>
                 </Container>
+            </div>
+            <div className={`${classes.mainRaised} ${classes.main}`}>
+                
+                <Container>
+                    <div className={`${classes.section}`}>
+                    <Grid container justify="center">
+                        <Grid item xs={12} sm={12} md={6}>
+                            <Typography variant="h3" component="h3" className={`${classes.productTitle}`}>Product</Typography>
+                            <Typography variant="body1" component="h5" className={`${classes.productDescription}`}>Desc</Typography>
+                        </Grid>
+                    </Grid>
+                    <div>
+                        <Grid container>
+                            <Grid item xs={12} sm={12} md={4}>
+                                <Card elevation={0}>
+                                    <CardContent>
+                                        <Typography variant="h4" component="h2" gutterBottom><BuildIcon fontSize="large" /></Typography>
+                                        <Typography variant="h5" component="h2" gutterBottom>Title</Typography>
+                                        <Typography variant="body1" component="p" gutterBottom>Paragraph</Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={4}>
+                                <Card elevation={0}>
+                                    <CardContent>
+                                        <Typography variant="h4" component="h2" gutterBottom><BuildIcon fontSize="large" /></Typography>
+                                        <Typography variant="h5" component="h2" gutterBottom>Title</Typography>
+                                        <Typography variant="body1" component="p" gutterBottom>Paragraph</Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={4}>
+                                <Card elevation={0}>
+                                    <CardContent>
+                                        <Typography variant="h4" component="h2" gutterBottom><BuildIcon fontSize="large" /></Typography>
+                                        <Typography variant="h5" component="h2" gutterBottom>Title</Typography>
+                                        <Typography variant="body1" component="p" gutterBottom>Paragraph</Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        </Grid>
+                    </div>
+                    </div>
+                </Container>
+                
+
+                
             </div>
         </>
     )
