cada47fb1020c8f35bda97a92e27fd4ec6c07310
ReisHaleem <Reis.Haleem@target.com>
2020-11-06 17:37:34 -0500

    add navbar button list

--- app/src/components/molecules/Navbar/Public/Navbar.tsx
+++ app/src/components/molecules/Navbar/Public/Navbar.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import NavbarTitle from "../../../atoms/NavbarTitle/NavbarTitle";
+import NavbarButtonList from "../../NavbarButtonList/NavbarButtonList";
 
 const Navbar: React.FC = () => {
     return (
         <AppBar position="static">
             <Toolbar>
                 <NavbarTitle title="The Exceptional Outliner" />
+                <NavbarButtonList names={["test1", "test2"]} />
             </Toolbar>
         </AppBar>
     );
