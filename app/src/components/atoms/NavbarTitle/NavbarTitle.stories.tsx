e5587c66a9b9b579f32e772be5f382181f7eb96a
ReisHaleem <Reis.Haleem@target.com>
2020-11-06 20:55:23 -0500

    Add link option to title and fix storybook to include react router links

--- app/src/components/atoms/NavbarTitle/NavbarTitle.stories.tsx
+++ app/src/components/atoms/NavbarTitle/NavbarTitle.stories.tsx
@@ -1,11 +1,13 @@
 import React, { ComponentProps } from "react";
 import { Story } from "@storybook/react/types-6-0";
+import { MemoryRouter } from "react-router-dom";
 
 import NavbarTitle from "./NavbarTitle";
 
 const storyDetails = {
     title: "NavbarTitle",
     component: NavbarTitle,
+    decorators: [(getStory: any) => <MemoryRouter>{getStory()}</MemoryRouter>],
 };
 export default storyDetails;
 
@@ -14,7 +16,13 @@ const Template: Story<ComponentProps<typeof NavbarTitle>> = (args) => {
 };
 
 // The default brand, with no additional args
-export const Default = Template.bind({});
-Default.args = {
-    title: "Default brand",
+export const WithoutLink = Template.bind({});
+WithoutLink.args = {
+    title: "Title that is not a link",
+};
+
+export const WithLink = Template.bind({});
+WithLink.args = {
+    title: "Title that is a link",
+    destination: "/",
 };
--- app/src/components/atoms/NavbarTitle/NavbarTitle.tsx
+++ app/src/components/atoms/NavbarTitle/NavbarTitle.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import makeStyles from "@material-ui/styles/makeStyles";
+import { Link } from "react-router-dom";
 
 interface PropTypes {
     title: string;
+    destination?: string;
 }
 
-const NavbarTitle: React.FC<PropTypes> = ({ title }) => {
+const useStyles = makeStyles(() => ({
+    titleLink: {
+        textDecoration: "none",
+        color: "inherit",
+    },
+}));
+
+const NavbarTitle: React.FC<PropTypes> = ({ title, destination }) => {
+    const classes = useStyles();
+
     return (
-        <Typography variant="h6" component="h2">
-            {title}
-        </Typography>
+        <>
+            {destination ? (
+                <Link to={destination} className={classes.titleLink}>
+                    <Typography variant="h6" component={"h2"}>
+                        {title}
+                    </Typography>
+                </Link>
+            ) : (
+                <Typography variant="h6" component={"h2"}>
+                    {title}
+                </Typography>
+            )}
+        </>
     );
 };
 
