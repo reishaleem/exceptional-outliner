80e0ba0329630da0787ba38b9811d1d88980493c
ReisHaleem <Reis.Haleem@target.com>
2020-11-21 20:59:09 -0500

    refactor recentlyeditedcard and add mock api fetches

---
+++ app/src/components/atoms/TabPanel/TabPanel.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface Props {
+    value: number;
+    index: number;
+    dir?: string;
+    children: React.ReactNode;
+}
+
+const TabPanel: React.FC<Props> = ({
+    value,
+    index,
+    children,
+    ...other
+}: Props) => {
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && children}
+        </div>
+    );
+};
+
+export default TabPanel;
--- app/src/components/organisms/RecentlyEditedCard/RecentlyEditedCard.tsx
+++ app/src/components/organisms/RecentlyEditedCard/RecentlyEditedCard.tsx
@@ -1,42 +1,127 @@
-import React from "react";
-import {
-    Avatar,
-    Box,
-    Card,
-    CardContent,
-    List,
-    ListItem,
-    ListItemAvatar,
-    ListItemText,
-    Paper,
-    Tab,
-    Tabs,
-    Typography,
-} from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Box from "@material-ui/core/Box";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Fab from "@material-ui/core/Fab";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import Paper from "@material-ui/core/Paper";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+import Typography from "@material-ui/core/Typography";
+import Zoom from "@material-ui/core/Zoom";
+import useTheme from "@material-ui/core/styles/useTheme";
+import AddIcon from "@material-ui/icons/Add";
+import { Link } from "react-router-dom";
 
-function TabPanel(props: any) {
-    const { children, value, index, ...other } = props;
+import TabPanel from "../../atoms/TabPanel/TabPanel";
 
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && <Box p={0}>{children}</Box>}
-        </div>
-    );
+interface RecentlyEditedWorld {
+    id: string;
+    name: string;
+    updatedAt: string;
+}
+
+interface RecentlyEditedPage {
+    id: string;
+    name: string;
+    updatedAt: string;
 }
 
 const RecentlyEditedCard: React.FC = () => {
-    const [recentlyEdtedTabsValue, setRecentlyEdtedTabsValue] = React.useState(
-        0
-    );
+    const [worlds, setWorlds] = useState<RecentlyEditedWorld[]>();
+    const [pages, setPages] = useState<RecentlyEditedPage[]>();
+    const [loading, setLoading] = useState<boolean>(true);
+    const [recentlyEditedTabsValue, setRecentlyEditedTabsValue] = useState<
+        number
+    >(0);
+    const theme = useTheme();
+
+    const transitionDuration = {
+        enter: theme.transitions.duration.enteringScreen,
+        exit: theme.transitions.duration.leavingScreen,
+    };
+
+    useEffect(() => {
+        // make api call to get worlds and pages then sort them
+        const worldsResponse: RecentlyEditedWorld[] = [
+            {
+                id: "1",
+                name: "World 1",
+                updatedAt: "November 21, 2020 at 7:34pm",
+            },
+            {
+                id: "2",
+                name: "World 2",
+                updatedAt: "November 21, 2020 at 7:34pm",
+            },
+            {
+                id: "3",
+                name: "World 3",
+                updatedAt: "November 21, 2020 at 7:34pm",
+            },
+        ];
+        const pagesResponse: RecentlyEditedPage[] = [
+            {
+                id: "1",
+                name: "Page 1",
+                updatedAt: "November 21, 2020 at 7:34pm",
+            },
+            {
+                id: "2",
+                name: "Page 2",
+                updatedAt: "November 21, 2020 at 7:34pm",
+            },
+            {
+                id: "3",
+                name: "Page 3",
+                updatedAt: "November 21, 2020 at 7:34pm",
+            },
+            {
+                id: "4",
+                name: "Page 3",
+                updatedAt: "November 21, 2020 at 7:34pm",
+            },
+            {
+                id: "5",
+                name: "Page 3",
+                updatedAt: "November 21, 2020 at 7:34pm",
+            },
+            {
+                id: "7",
+                name: "Page 3",
+                updatedAt: "November 21, 2020 at 7:34pm",
+            },
+        ];
+
+        worldsResponse.sort((a, b) => {
+            const d1 = new Date(a.updatedAt);
+            const d2 = new Date(b.updatedAt);
+            if (d1 >= d2) {
+                return -1;
+            } else {
+                return 1;
+            }
+        });
+        pagesResponse.sort((a, b) => {
+            const d1 = new Date(a.updatedAt);
+            const d2 = new Date(b.updatedAt);
+            if (d1 >= d2) {
+                return -1;
+            } else {
+                return 1;
+            }
+        });
+        setWorlds(worldsResponse);
+        setPages(pagesResponse);
+        setLoading(false);
+    }, []);
 
     const handleRecentlyEditedTabsChange = (event: any, newValue: any) => {
-        setRecentlyEdtedTabsValue(newValue);
+        setRecentlyEditedTabsValue(newValue);
     };
 
     return (
@@ -48,7 +133,7 @@ const RecentlyEditedCard: React.FC = () => {
             </CardContent>
             <Paper square elevation={0}>
                 <Tabs
-                    value={recentlyEdtedTabsValue}
+                    value={recentlyEditedTabsValue}
                     indicatorColor="primary"
                     textColor="primary"
                     onChange={handleRecentlyEditedTabsChange}
@@ -59,68 +144,138 @@ const RecentlyEditedCard: React.FC = () => {
                     <Tab label="Pages" />
                 </Tabs>
             </Paper>
-            <TabPanel value={recentlyEdtedTabsValue} index={0}>
-                <List>
-                    <ListItem button>
-                        <ListItemAvatar>
-                            <Avatar>I</Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary="World Name"
-                            secondary="Updated time"
-                        />
-                    </ListItem>
-                    <ListItem button>
-                        <ListItemAvatar>
-                            <Avatar>I</Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary="World Name"
-                            secondary="Updated time"
-                        />
-                    </ListItem>
-                    <ListItem button>
-                        <ListItemAvatar>
-                            <Avatar>I</Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary="World Name"
-                            secondary="Updated time"
-                        />
-                    </ListItem>
-                </List>
-            </TabPanel>
-            <TabPanel value={recentlyEdtedTabsValue} index={1}>
-                <List>
-                    <ListItem button>
-                        <ListItemAvatar>
-                            <Avatar>W</Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary="Page Name"
-                            secondary="Updated time"
-                        />
-                    </ListItem>
-                    <ListItem button>
-                        <ListItemAvatar>
-                            <Avatar>W</Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary="Page Name"
-                            secondary="Updated time"
-                        />
-                    </ListItem>
-                    <ListItem button>
-                        <ListItemAvatar>
-                            <Avatar>W</Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary="Page Name"
-                            secondary="Updated time"
-                        />
-                    </ListItem>
-                </List>
-            </TabPanel>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <>
+                    <TabPanel
+                        value={recentlyEditedTabsValue}
+                        index={0}
+                        dir={theme.direction}
+                    >
+                        <Box height={250} position="relative">
+                            {worlds!.length > 0 ? (
+                                <Box overflow="auto">
+                                    <List>
+                                        {worlds!.map((world) => {
+                                            return (
+                                                <ListItem button>
+                                                    <ListItemAvatar>
+                                                        <Avatar
+                                                            src="World image"
+                                                            alt={world.name[0]}
+                                                        />
+                                                    </ListItemAvatar>
+                                                    <ListItemText
+                                                        primary={world.name}
+                                                        secondary={
+                                                            world.updatedAt
+                                                        }
+                                                    />
+                                                </ListItem>
+                                            );
+                                        })}
+                                    </List>
+                                </Box>
+                            ) : (
+                                <p>No worlds created</p>
+                            )}
+                            <Zoom
+                                in={recentlyEditedTabsValue === 0}
+                                timeout={transitionDuration}
+                                style={{
+                                    transitionDelay: `${
+                                        recentlyEditedTabsValue === 0
+                                            ? transitionDuration.exit
+                                            : 0
+                                    }ms`,
+                                }}
+                                unmountOnExit
+                            >
+                                <Fab
+                                    color="primary"
+                                    aria-label="add-world"
+                                    size="medium"
+                                    style={{
+                                        position: "absolute",
+                                        bottom: theme.spacing(2),
+                                        right: theme.spacing(2),
+                                    }}
+                                    component={Link}
+                                    to="/worlds/new"
+                                >
+                                    <AddIcon />
+                                </Fab>
+                            </Zoom>
+                        </Box>
+                    </TabPanel>
+                    <TabPanel
+                        value={recentlyEditedTabsValue}
+                        index={1}
+                        dir={theme.direction}
+                    >
+                        <Box position="relative" height={250}>
+                            {pages!.length > 0 ? (
+                                <>
+                                    <Box overflow="auto">
+                                        <List>
+                                            {pages!.map((page) => {
+                                                return (
+                                                    <ListItem button>
+                                                        <ListItemAvatar>
+                                                            <Avatar
+                                                                src="Page image"
+                                                                alt={
+                                                                    page.name[0]
+                                                                }
+                                                            />
+                                                        </ListItemAvatar>
+                                                        <ListItemText
+                                                            primary={page.name}
+                                                            secondary={
+                                                                page.updatedAt
+                                                            }
+                                                        />
+                                                    </ListItem>
+                                                );
+                                            })}
+                                        </List>
+                                    </Box>
+                                </>
+                            ) : (
+                                <p>No pages created</p>
+                            )}
+                            <Zoom
+                                in={recentlyEditedTabsValue === 1}
+                                timeout={transitionDuration}
+                                style={{
+                                    transitionDelay: `${
+                                        recentlyEditedTabsValue === 1
+                                            ? transitionDuration.exit
+                                            : 0
+                                    }ms`,
+                                }}
+                                unmountOnExit
+                            >
+                                <Fab
+                                    color="primary"
+                                    aria-label="add-page"
+                                    size="medium"
+                                    style={{
+                                        position: "absolute",
+                                        bottom: theme.spacing(2),
+                                        right: theme.spacing(2),
+                                    }}
+                                    component={Link}
+                                    to="/pages/new"
+                                >
+                                    <AddIcon />
+                                </Fab>
+                            </Zoom>
+                        </Box>
+                    </TabPanel>
+                </>
+            )}
         </Card>
     );
 };
